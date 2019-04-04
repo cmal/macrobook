d90831c35d97e2995f842483fd9d0ba6a4e7f7ea
Yu Zhao <yu.zhao@joudou.com>
2019-04-04 12:41:55 +0800

    init

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject macrobook "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [refactor-nrepl "2.3.1"]]
+  :main ^:skip-aot macrobook.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
